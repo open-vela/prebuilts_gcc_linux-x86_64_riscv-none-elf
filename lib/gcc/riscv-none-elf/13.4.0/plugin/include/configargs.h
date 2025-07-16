1fd3944850ab0c65a7cb693aaef85a75cfb42496
Shanmin Zhang <zhangshanmin@xiaomi.com>
2025-07-17 01:48:34 +0800

    GCC: update to version to 13.4.0 (riscv-none-elf) 20250718
    Signed-off-by: Shanmin Zhang <zhangshanmin@xiaomi.com>

---
+++ lib/gcc/riscv-none-elf/13.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/shanmin/gcc-build/gcc/configure --prefix=/home/shanmin/gcc-build/out/linux-x86_64/riscv-none-elf --host=x86_64-unknown-linux-gnu --build=x86_64-unknown-linux-gnu --target=riscv-none-elf --with-zstd=/home/shanmin/gcc-build/out/linux-x86_64/riscv-none-elf --with-gmp=/home/shanmin/gcc-build/out/linux-x86_64/riscv-none-elf --with-mpfr=/home/shanmin/gcc-build/out/linux-x86_64/riscv-none-elf --with-mpc=/home/shanmin/gcc-build/out/linux-x86_64/riscv-none-elf --with-gnu-as --with-gnu-ld --disable-werror --disable-shared --disable-nls --disable-threads --disable-tls --with-system-zlib --enable-gnu-unique-object --enable-linker-build-id --with-newlib --with-sysroot=/home/shanmin/gcc-build/out/linux-x86_64/riscv-none-elf/riscv-none-elf --with-native-system-header-dir=/include --with-headers=yes --enable-languages=c,c++,lto --enable-checking=release";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "ilp32d" }, { "arch", "rv32imafdc_zicsr_zifencei" }, { "isa_spec", "20191213" } };
